41694a53d930329d99c90c539bc1a6fb854f137d
Dastan <dastan.zhabagin@kaspi.kz>
2025-04-29 15:23:33 +0500

    ✨ Implement metrics tracking and reset functionality in the API

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func main() {
+	const filePathRoot = "."
+	const port = "8080"
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))))
+	mux.HandleFunc("/healthz", HealthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
+	log.Fatal(server.ListenAndServe())
+}
 
 func HealthzHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -14,18 +42,24 @@ func HealthzHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
-	const filePathRoot = "."
-	const port = "8080"
-
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
-	mux.HandleFunc("/healthz", HealthzHandler)
-
-	server := &http.Server{
-		Addr:    ":" + port,
-		Handler: mux,
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
 	}
-	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
-	log.Fatal(server.ListenAndServe())
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
 }
