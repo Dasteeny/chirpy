47b49c82e190c886e08b185e804403259f33575f
Dasteeny <dastan.zhabagin@gmail.com>
2025-05-10 09:44:23 +0500

    ✨ feat: implement ListChirpsByAuthor functionality to retrieve chirps by user ID

--- handler_chirps_list.go
+++ handler_chirps_list.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/dasteeny/chirpy/internal/database"
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerListChirps(w http.ResponseWriter, r *http.Request) {
-	chirps, err := cfg.db.ListChirps(r.Context())
+	author_id_str := r.URL.Query().Get("author_id")
+	userID, err := uuid.Parse(author_id_str)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps list", err)
-		return
+		respondWithError(w, http.StatusInternalServerError, "Invalid author ID", err)
+	}
+
+	chirps := []database.Chirp{}
+	if userID == uuid.Nil {
+		chirps, err = cfg.db.ListChirps(r.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps list", err)
+			return
+		}
+	} else {
+		chirps, err = cfg.db.ListChirpsByAuthor(r.Context(), userID)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps list", err)
+			return
+		}
 	}
 
 	chirpsList := []Chirp{}
