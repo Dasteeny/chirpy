284a39d5788cac54ac5fcf3bd58aea86a90e34a6
Dasteeny <dastan.zhabagin@gmail.com>
2025-05-10 12:56:36 +0500

    ♻️  refactor: remove ListChirpsByAuthor functionality and update handler to filter chirps by author ID

--- handler_chirps_list.go
+++ handler_chirps_list.go
@@ -3,42 +3,40 @@ package main
 import (
 	"net/http"
 
-	"github.com/dasteeny/chirpy/internal/database"
 	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerListChirps(w http.ResponseWriter, r *http.Request) {
-	author_id_str := r.URL.Query().Get("author_id")
-	userID, err := uuid.Parse(author_id_str)
+func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
+	dbChirps, err := cfg.db.ListChirps(r.Context())
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Invalid author ID", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+		return
 	}
 
-	chirps := []database.Chirp{}
-	if userID == uuid.Nil {
-		chirps, err = cfg.db.ListChirps(r.Context())
+	authorID := uuid.Nil
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err = uuid.Parse(authorIDString)
 		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps list", err)
-			return
-		}
-	} else {
-		chirps, err = cfg.db.ListChirpsByAuthor(r.Context(), userID)
-		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps list", err)
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
 			return
 		}
 	}
 
-	chirpsList := []Chirp{}
-	for _, chirp := range chirps {
-		chirpsList = append(chirpsList, Chirp{
-			ID:        chirp.ID,
-			CreatedAt: chirp.CreatedAt,
-			UpdatedAt: chirp.UpdatedAt,
-			Body:      chirp.Body,
-			UserID:    chirp.UserID,
+	chirps := []Chirp{}
+	for _, dbChirp := range dbChirps {
+		if authorID != uuid.Nil && dbChirp.UserID != authorID {
+			continue
+		}
+
+		chirps = append(chirps, Chirp{
+			ID:        dbChirp.ID,
+			CreatedAt: dbChirp.CreatedAt,
+			UpdatedAt: dbChirp.UpdatedAt,
+			UserID:    dbChirp.UserID,
+			Body:      dbChirp.Body,
 		})
 	}
 
-	respondWithJSON(w, http.StatusOK, chirpsList)
+	respondWithJSON(w, http.StatusOK, chirps)
 }
